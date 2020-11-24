565b285bc44ef1ef1a77d7a717d1d65af365be0a
Maksym Yarmolenko <maksym.yarmolenk@gmail.com>
2020-11-24 19:39:58 +0200

    New test

--- main.c
+++ main.c
@@ -1,10 +1,14 @@
 #include <stdio.h>
 #include "op_lib.h"
+//#include <gtest/gtest.h>
 
 int main()
 {
 
-    printf("%d", IMPL(1, 1));
+    NOT(1); // 0
+    NOT(0); // 1
+
+    OR(2, 0); // 1
 
     return 0;
 }
