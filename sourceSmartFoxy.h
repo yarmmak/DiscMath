068857abf44b31d941873d2f63fb3b62066f6d00
Maksym Yarmolenko <maksym.yarmolenk@gmail.com>
2021-01-12 18:30:28 +0200

    Added src files

---
+++ sourceSmartFoxy.h
@@ -0,0 +1,13 @@
+//
+// Created by Максим on 12.01.2021.
+//
+
+#ifndef DISCMATH_SOURCESMARTFOXY_H
+#define DISCMATH_SOURCESMARTFOXY_H
+#include "sourceSmartFoxy.c"
+
+
+int postfixNotation(char *ptr);
+void cartesianProduct();
+
+#endif //DISCMATH_SOURCESMARTFOXY_H
